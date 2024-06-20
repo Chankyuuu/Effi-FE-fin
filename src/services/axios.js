e2811d6a69f43851b1731311424cf20887928ab7
chankyuu <shin4438@gmail.com>
2024-06-20 16:37:06 +0900

    Fix : localhost to ec2

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import { useAuthStore } from '@/stores/auth.js';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://43.203.76.140:8080',
     timeout: 5000,
 });
 
