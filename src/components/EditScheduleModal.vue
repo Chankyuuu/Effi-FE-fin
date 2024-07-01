d233fe6dc523d3cd17c802e248c1b2d5e02cfe12
chankyuu <shin4438@gmail.com>
2024-07-01 12:11:59 +0900

    Fix : change no to num

--- src/components/EditScheduleModal.vue
+++ src/components/EditScheduleModal.vue
@@ -2,7 +2,7 @@
   <div v-if="show" class="modal-overlay">
     <div class="modal-container">
       <button class="close-button" @click="closeModal">✕</button>
-        <template v-if="categoryNo.value === 1 || categoryNo.value === 2">
+        <template v-if="categoryNum.value === 1 || categoryNum.value === 2">
           <h2 v-if="rank === '부장' || rank === '차장'">일정 수정하기</h2>
           <h2 v-else>일정 상세보기</h2>
         </template>
@@ -103,7 +103,7 @@
                 v-model="internalEvent.notificationYn"></label>
           </div>
           <div class="modal-footer">
-            <template v-if="categoryNo.value === 1 || categoryNo.value === 2">
+            <template v-if="categoryNum.value === 1 || categoryNum.value === 2">
               <template v-if = "rank === '부장' || rank === '차장'">
                 <button type="submit" class="update-button">수정</button>
                 <button type="button" class="delete-button" @click="deleteSchedule">삭제</button>
@@ -170,7 +170,7 @@ export default {
     const tagMap = ref({});  // 태그 이름과 ID를 매핑하기 위한 객체
     const existingTags = ref([]); // 기존 태그를 저장하는 배열
     const rank = useAuthStore().rank;
-    const categoryNo = ref(null);
+    const categoryNum = ref(null);
 
     onMounted(() => {
       if (props.scheduleId) {
@@ -207,7 +207,7 @@ export default {
           routineId: schedule.routineId,
           routineCycle: schedule.routineCycle,
         };
-        categoryNo.value = schedule.categoryNo;
+        categoryNum.value = schedule.categoryNo;
 
         existingTags.value = internalEvent.value.tags.slice(); // 기존 태그를 저장
 
@@ -564,7 +564,7 @@ export default {
       getRandomColor,
       removeTag,
       rank,
-      categoryNo
+      categoryNum
     };
   }
 };
